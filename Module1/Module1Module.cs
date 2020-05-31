6a9f84acdbfa8b9f23733958a690a5e7322c84c0
HIRONOBU <ttlatex@yahoo.co.jp>
2020-05-31 22:03:19 +0900

    モデルの生成タイミング変更

--- Module1/Module1Module.cs
+++ Module1/Module1Module.cs
@@ -10,7 +10,7 @@ namespace Module1
     {
         public void OnInitialized(IContainerProvider containerProvider)
         {
-
+            _ = containerProvider.Resolve<ProgressDialogModel>();
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
